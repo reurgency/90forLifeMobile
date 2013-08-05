abdfa366caf723e533730524a40623ce60d390df
rePassante <rob.passante@reurgency.com>
2013-08-05 13:50:16 -0400

    updated environment & URL's

--- www/js/config.js
+++ www/js/config.js
@@ -2,16 +2,16 @@
  * Environment variable for the remote server. This is used to select the appropriate remote URL
  * @type {string} Environment Name
  */
-const RemoteServerEnvironment = "Prod";
+const RemoteServerEnvironment = "Dev";
 
 /**
  * Remote URL Variables. One for each possible RemoteServerEnvironment value. Must Be named like
  * RemoteServerEnvironment + "_RemoteURL"
  */
-const Dev_RemoteURL     = "http://www.youngevity.reurgency.com/90forLifeMobile#/";
+const Dev_RemoteURL     = "http://www.youngevity.reurgency.com/90forLifeMobile";
 const QA_RemoteURL      = "";
 const Staging_RemoteURL = "http://appstest.youngevity.com/90forLifeMobile/";
-const Demo_RemoteURL    = "http://www.reurgency.com/#/Demo";
+const Demo_RemoteURL    = "http://demo.youngevity.reurgency.com/90forLifeMobile";
 const Prod_RemoteURL    = "https://apps.youngevity.com/90ForLifeMobile/";
 //END REMOTE URL VARIABLES
 
